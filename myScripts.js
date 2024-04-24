0ed165fc51dcf3088481d2a36351ea1d34d02513
276518 <157829135+276518@users.noreply.github.com>
2024-04-24 11:53:48 -0500

    apr24

--- myScripts.js
+++ myScripts.js
@@ -14,4 +14,15 @@ function startTime() {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
    }
-   
+   //CALCULATOR (JAVASCRIPT)
+var showCalc = false;
+function showCalculator(){
+   if (showCalc == false){
+       document.getElementById("calc").style.opacity = 1;
+       showCalc = true;
+   }
+   else if (showCalc == true){
+       document.getElementById("calc").style.opacity = 0;
+       showCalc = false;
+   }
+}
